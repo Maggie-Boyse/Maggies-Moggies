9d8493b56786f7c23066990608a3ce1824539c29
Maggie Boyse <maggie_boyse@outlook.com>
2024-04-28 14:51:04 -0400

    0.2 debug, basic header styling

--- src/COMPONENTS/Header/Header.jsx
+++ src/COMPONENTS/Header/Header.jsx
@@ -1,10 +1,26 @@
 import "../Header/Header.scss";
-    
+import logo from "../../assets/images/MM-logo.jpg";
+// import { Link } from "react-router-dom";
+
 function Header() {
   return (
-    <div className="">
-      <p>Header</p>
-    </div>
+    <header className="header">
+      <div className="header__logo-title">
+        {/* <Link to="/"> */}
+        <img
+          src={logo}
+          path="/"
+          alt="Maggie's Moggies Logo"
+          className="header__logo"
+        />
+        {/* </Link> */}
+        <h1 className="header__title">Maggie's Moggies</h1>
+      </div>
+      <div className="header__signs">
+        <button className="header__sign-in">Sign In</button>
+        <button className="header__sign-up">Sign Up</button>
+      </div>
+    </header>
   );
 }
 
