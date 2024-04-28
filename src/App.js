4c51021ba0a60f5f398633383fa281bdf1c10aeb
Maggie Boyse <maggie_boyse@outlook.com>
2024-04-28 15:17:40 -0400

    0.0 create and add navigation bar to app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./COMPONENTS/Header/Header";
 import Footer from "./COMPONENTS/Footer/Footer";
+import NavBar from "./COMPONENTS/NavBar/NavBar";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Header />
+        <NavBar />
         <Footer />
       </header>
     </div>
--- src/COMPONENTS/NavBar/NavBar.jsx
+++ src/COMPONENTS/NavBar/NavBar.jsx
@@ -1,10 +1,17 @@
 import "../NavBar/NavBar.scss";
-    
+
 function NavBar() {
   return (
-    <div className="">
-      <>NavBar</>
-    </div>
+    <nav className="navbar">
+      <ul className="navbar__list">
+        <li className="navbar__item">About</li>
+        <li className="navbar__item">Learn to Crochet</li>
+        <li className="navbar__item">Community Board</li>
+        <li className="navbar__item">Search Patterns</li>
+        {/* upload pattern modal or whatever, not new page */}
+        {/* <li>Pattern Builder</li> */}
+      </ul>
+    </nav>
   );
 }
 
