0bd8a9986848ada795befacb81c63b643984acbb
Maggie Boyse <maggie_boyse@outlook.com>
2024-04-28 18:14:55 -0400

    0.1 work out chaining component

--- src/COMPONENTS/Chaining/Chaining.jsx
+++ src/COMPONENTS/Chaining/Chaining.jsx
@@ -9,17 +9,63 @@ function Chaining() {
         <p className="chaining__text">
           {" "}
           To begin, we will hold our yarn at one end, in a loop. Make sure your
-          tail is <span>behind</span>! See image one.
+          tail is <span className="chaining__span">behind</span>! Pop your hook
+          through the loop you made. <br />
+          <span className="chaining__span">
+            Note, finding a comfy way to hold the yarn is highly individualized.
+            Pictured is my preferred method.
+          </span>
         </p>
-      </div>
-      <div className="chaining__image">
         <img
           src={placeholder}
           // don't forget to update src and alt
           alt="placeholder"
-          className="chaining__image-one"
+          className="chaining__image-1"
           // replace with proper class name later
         />
+      </div>
+
+      <div className="chaining__slipknot-2">
+        <img
+          src={placeholder}
+          // don't forget to update src and alt
+          alt="placeholder"
+          className="chaining__image-2"
+          // replace with proper class name later
+        />
+        <p className="chaining__text">
+          Then, catch your trailing yarn, and pull it back{" "}
+          <span className="chaining__span">through</span> the loop you made,
+          pulling snug.
+        </p>
+      </div>
+
+      <div className="chaining__slipknot-3">
+        <p className="chaining__text">
+          Now, keeping our hook snug inside the loop, we'll grasp the knot
+          between finger and thumb. Catch the trailing yarn once more, and pull
+          through the loop.
+        </p>
+        <img
+          src={placeholder}
+          // don't forget to update src and alt
+          alt="placeholder"
+          className="chaining__image-3"
+          // replace with proper class name later
+        />
+      </div>
+
+      <div className="chaining__slipknot-4">
+        <img
+          src={placeholder}
+          // don't forget to update src and alt
+          alt="placeholder"
+          className="chaining__image-4"
+          // replace with proper class name later
+        />
+        <p className="chaining__text">
+          Repeat until desired length of project!
+        </p>
       </div>
     </section>
   );
