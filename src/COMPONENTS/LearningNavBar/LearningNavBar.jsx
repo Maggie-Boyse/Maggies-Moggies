fbbd5130f4aa77fb8ce64773b74a7c30676a0ab3
Maggie Boyse <maggie_boyse@outlook.com>
2024-04-29 16:04:59 -0400

    0.0 create branch

--- src/COMPONENTS/LearningNavBar/LearningNavBar.jsx
+++ src/COMPONENTS/LearningNavBar/LearningNavBar.jsx
@@ -2,8 +2,22 @@ import "../LearningNavBar/LearningNavBar.scss";
     
 function LearningNavBar() {
   return (
-    <div className="">
-      <>LearningNavBar</>
+    <div className="navbar">
+      <ul className="navbar__list">
+        <Link to="/" className="navbar__item">
+          About
+        </Link>
+        <Link to="/learn" className="navbar__item">
+          Learn to Crochet
+        </Link>
+        {/* <li className="navbar__item" component={Link} to="/learn">
+          Learn to Crochet
+        </li> */}
+        <li className="navbar__item">Community Board</li>
+        <li className="navbar__item">Search Patterns</li>
+        {/* upload pattern modal or whatever, not new page */}
+        {/* <li>Pattern Builder</li> */}
+      </ul>
     </div>
   );
 }
--- src/PAGES/LearningPage/LearningPage.js
+++ src/PAGES/LearningPage/LearningPage.js
@@ -5,12 +5,14 @@ import Footer from "../../COMPONENTS/Footer/Footer";
 import Chaining from "../../COMPONENTS/Chaining/Chaining";
 import SingleCrochet from "../../COMPONENTS/SingleCrochet/SingleCrochet";
 import DoubleCrochet from "../../COMPONENTS/DoubleCrochet/DoubleCrochet";
+import LearningNavBar from "../../COMPONENTS/LearningNavBar/LearningNavBar";
 
 function LearningPage() {
   return (
     <div>
       <Header />
       <NavBar />
+      <LearningNavBar />
       <Chaining />
       <SingleCrochet />
       <DoubleCrochet />
