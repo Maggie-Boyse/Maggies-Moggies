4ad81f5becce476da198426015065f557d1b5c67
Maggie Boyse <maggie_boyse@outlook.com>
2024-05-05 12:15:00 -0400

    0.2 basic search result list code, actually a little sus

--- src/PAGES/PatternPage/PatternPage.jsx
+++ src/PAGES/PatternPage/PatternPage.jsx
@@ -2,32 +2,33 @@ import "../PatternPage/PatternPage.scss";
 import Header from "../../COMPONENTS/Header/Header";
 import NavBar from "../../COMPONENTS/NavBar/NavBar";
 import SearchPatterns from "../../COMPONENTS/SearchPatterns/SearchPatterns";
-import PatternItem from "../../COMPONENTS/PatternItem/PatternItem";
+import SearchResult from "../../COMPONENTS/SearchResultList/SearchResultList";
+// import PatternItem from "../../COMPONENTS/PatternItem/PatternItem";
 import Footer from "../../COMPONENTS/Footer/Footer";
 import { Link } from "react-router-dom";
-import axios from "axios";
-import { API_URL } from "../../utils/api";
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+// import axios from "axios";
+// import { API_URL } from "../../utils/api";
+// import { useEffect, useState } from "react";
+// import { useParams } from "react-router-dom";
 
 function PatternPage() {
-  const { patternId } = useParams();
-  const [patternsData, setPatternsData] = useState([]);
-  const [patternList, setPatternList] = useState([]);
+  // const { patternId } = useParams();
+  // const [patternsData, setPatternsData] = useState([]);
+  // const [patternList, setPatternList] = useState([]);
 
-  const fetchPatterns = async () => {
-    const patternsReq = await axios.get(`${API_URL}/patterns`);
-    const patternsData = patternsReq.data;
-    setPatternList(patternsData);
-    setPatternsData(patternsData);
-  };
-  useEffect(() => {
-    fetchPatterns();
-  }, [patternId]);
-  if (patternsData.length === 0) {
-    return <p>... Loading Pattern details ...</p>;
-  }
-  const patternToLoad = patternId ? patternId : patternList[0].id;
+  // const fetchPatterns = async () => {
+  //   const patternsReq = await axios.get(`${API_URL}/patterns`);
+  //   const patternsData = patternsReq.data;
+  //   setPatternList(patternsData);
+  //   setPatternsData(patternsData);
+  // };
+  // useEffect(() => {
+  //   fetchPatterns();
+  // }, [patternId]);
+  // if (patternsData.length === 0) {
+  //   return <p>... Loading Pattern details ...</p>;
+  // }
+  // const patternToLoad = patternId ? patternId : patternList[0].id;
 
   return (
     <div className="patterns">
@@ -35,17 +36,19 @@ function PatternPage() {
       <NavBar />
       <h1 className="patterns__title">Search and Upload Patterns</h1>
       <SearchPatterns />
+      <SearchResult />
       <Link to="/upload" className="patterns__upload">
         Upload Here{" "}
       </Link>
 
-      {patternsData.map((pattern) => (
+      {/* {patternsData.map((pattern) => (
         <PatternItem
           key={pattern.id}
           pattern={pattern}
           patternToLoad={patternToLoad}
         />
-      ))}
+      ))} */}
+
       <Footer />
     </div>
   );
